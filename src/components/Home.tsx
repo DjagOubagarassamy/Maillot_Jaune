b9f420238880dc6a66dc95b3da8a0c31b5eaf964
Djag <oubadjag@gmail.com>
2026-03-29 19:54:41 +0200

    key: Final commit, add: README.md final

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -28,7 +28,7 @@ export function Home({ onNavigate }: HomeProps) {
     <div className="bg-gray-950">
       <div className="relative h-screen">
         <video autoPlay loop muted playsInline className="absolute inset-0 w-full h-full object-cover">
-          <source src='/video_home_hd.mp4' type='video/mp4'/>
+          <source src='/Maillot_Jaune/video_home_hd.mp4' type='video/mp4'/>
         </video>
         <div className="absolute inset-0 bg-black/40" />
         <div className="relative z-10 flex flex-col items-center justify-center h-full text-center px-4">
